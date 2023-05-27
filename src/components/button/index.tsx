19b71fc580b5e2bd685a6f32780c26d92723b547
mariana <marianalais.eng@gmail.com>
2023-05-26 21:41:43 -0300

    updates

---
+++ src/components/button/index.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+interface ButtonProps {
+  text: string
+  onclick: () => void
+	hierarchy?: 'primary'| 'secondary-color'| 'secondary-gray'	| 'secondary-blue'
+}
+
+
+export default function Button({text,onclick,hierarchy='primary'}:ButtonProps) {
+  return (
+    <div className="App">
+      <button onClick={()=> onclick()}>{text}</button>
+    </div>
+  );
+}
