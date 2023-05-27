92d39960efd630672ba5027c194c285cbc450a14
William Barrilli <felip.ew@hotmail.com>
2023-05-27 16:18:12 -0300

    add input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,3 +1,13 @@
+// recebe parametro label =
+// text= acima do input,
+// value= valor do input
+//onChange = função para quando for digitado
+// placeholder
+
 export default function Input() {
-  return <input>qual seu nome</input>;
+  return (
+    <div>
+      <input type="text" name="input" value=""></input>
+    </div>
+  );
 }
