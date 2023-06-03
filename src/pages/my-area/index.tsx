e97074cf18e97a6b5ae739c637853970f73a1fb4
mariana <marianalais.eng@gmail.com>
2023-06-03 14:35:08 -0300

    ajustando modal my-area

--- src/pages/my-area/index.tsx
+++ src/pages/my-area/index.tsx
@@ -16,6 +16,32 @@ export default function MyArea() {
   const [dateSelected, setDateSelected] = useState<Moment | null>(null);
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
 
+  const renderTableBody = () => {
+    return horarios.map((horario, index) => {
+      const filterHour = filterList.find(
+        (reserved: Reserved) => reserved.hour === horario
+      );
+      return (
+        <tr>
+          <td>{horario}</td>
+          <td>{filterHour?.name ? filterHour.name : "livre"}</td>
+          <td>{filterHour?.phone ? filterHour.phone : ""}</td>
+        </tr>
+      );
+    });
+  };
+
+  const horarios = [
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+  ];
+
   const fetchData = async () => {
     try {
       const solicitationList = await getSolicitationList(
@@ -72,28 +98,15 @@ export default function MyArea() {
         isOpen={isOpenModal}
         onClose={() => setIsOpenModal(false)}
       >
-        <div>
-          <h1 className={styles.text}>
-            Horários do dia: {dateSelected?.format("DD/MM/YYYY")}
-          </h1>
-          <div className={styles.grid}>
-            {filterList.length ? (
-              filterList.map((reserved, index) => (
-                <div key={index} className={styles.horarioContainer}>
-                  <p className={styles.textHorarios}>
-                    Horário: {reserved.hour}
-                  </p>
-
-                  <p className={styles.green}>{reserved.name}</p>
-                </div>
-              ))
-            ) : (
-              <p className={styles.textHorarios}>
-                Sem horarios agendados para esta data
-              </p>
-            )}
-          </div>
-        </div>
+        {/* <h1> Horarios do dia: {dateSelected?.format("DD/MM/YYYY")} </h1> */}
+        <table>
+          <thead>
+            <th>Horário</th>
+            <th>Nome</th>
+            <th>Contato</th>
+          </thead>
+          <tbody>{renderTableBody()}</tbody>
+        </table>
       </ModalComponent>
     </div>
   );
