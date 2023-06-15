05c94d86f1514d22ea452bdaf14a458f638838be
William <william.barrilli@meta.com.br>
2023-06-15 13:16:35 -0300

    update css and icon home

--- src/views/home/buttonsView/index.tsx
+++ src/views/home/buttonsView/index.tsx
@@ -1,48 +1,44 @@
 import Button from "../../../components/button";
 import { Shop } from "../../../types/shop";
 import { sendMessage } from "../../../utils/send-message-whats-app";
-import "./styles.css";
 import { useNavigate } from "react-router-dom";
 import instagram from "../../../assets/icons/instagram.svg";
+import styles from "./styles.module.scss";
 
 export default function ButtonsView({ shop }: { shop: Shop }) {
   const navigate = useNavigate();
   const message =
     "Olá, tenho interesse em saber mais sobre os serviços oferecidos!";
+
   const redirectToInstagram = () => {
     // Substitua "nome_do_perfil" pelo nome de usuário do perfil do Instagram
     const instagramURL = `https://www.instagram.com/${shop.instagram}`;
     window.open(instagramURL, "_blank");
   };
+
   return (
-    <div className="container-buttons">
+    <div className={styles["container-buttons"]}>
       <div>
-        <div className="divider"></div>
-        <div className="content-buttons">
+        <div className={styles.divider} />
+        <div className={styles["content-buttons"]}>
           <Button
             text="Agendar"
             size="lg"
             onclick={() => navigate("/agendar")}
           />
         </div>
-        {/* <div className="content-buttons">
-          <Button
-            size="lg"
-            text="Reservas"
-            onclick={() => console.log("reservas")}
-          />
-        </div> */}
-        <div className="content-buttons">
+
+        <div className={styles["container-buttons"]}>
           <Button
             size="lg"
             text="Contato"
             onclick={() => sendMessage(message, shop.phone)}
           />
         </div>
-        <div className="divider"></div>
-        <div className="content-icons">
+        <div className={styles.divider} />
+        <div className={styles["content-icons"]}>
           <img
-            className="iconInstagram"
+            className={styles["iconInstagram"]}
             onClick={redirectToInstagram}
             src={instagram}
             alt="instagram"
