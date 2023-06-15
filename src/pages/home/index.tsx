32fab210132ae0f771d8e7c6d212d9749277c1bb
mariana <marianalais.eng@gmail.com>
2023-06-15 13:09:55 -0300

    ajuste css home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import CardComponent from "../../components/card";
 import { useGetShopsListHook } from "../../hook/getShopsList";
+import styles from "./styles.module.scss";
 
 export default function Home() {
   const { data, isLoading, error } = useGetShopsListHook();
@@ -7,12 +8,12 @@ export default function Home() {
   if (isLoading) return <>carregando...</>;
   if (error) return <>Ocorreu um erro inesperado</>;
   return (
-    <>
+    <div className={styles.container}>
       {data?.map((loja, index) => (
         <div key={index}>
           <CardComponent image={loja.url} title={loja.name} url={loja.url} />
         </div>
       ))}
-    </>
+    </div>
   );
 }
